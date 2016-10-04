a832b18213b8f95a15a503b8904ec87d7d59a607
VelizarVarbanov <velizarvarbanov@gmail.com>
2016-10-04 12:44:15 +0300

    initial commit

--- 4_Arrays_Strings_SourceControl/src/Task1c_PrintMatrix.java
+++ 4_Arrays_Strings_SourceControl/src/Task1c_PrintMatrix.java
@@ -8,38 +8,54 @@ public class Task1c_PrintMatrix {
         int row = 0;
         int col = 0;
         int[][] arr = new int[n][n];
-//        while (k < n * n) {
-//            arr[row][col] = k;
-//            k++;
-//            col++;
-//            arr[row][col] = k;
-//            if (row == 0 && col != 0) {
-//                while (col != 0) {
-//                    col--;
-//                    row++;
-//                    arr[row][col] = k;
-//                    k++;
-//                }
-//            }
-//            if (col == 0 && row != 0) {
-//                row++;
-//                arr[row][col] = k;
-//                while (row != 0) {
-//                    col++;
-//                    row--;
-//                    arr[row][col] = k;
-//                    k++;
-//                }
-//            }
-//        }
-//        for ( row = 0; row < arr.length; row++) {
-//            for ( col = 0; col < arr.length; col++) {
-//                System.out.print(arr[row][col]);
-//
-//            }
-//            System.out.println();
-//
-//        }
-// TODO
+        boolean directioUp = true;
+        arr[0][0] = 1;
+        while (k < n * n) {
+            if(directioUp) {
+                if (row == 0 && col < arr.length - 1) {
+                    col++;
+                    k++;
+                    directioUp = false;
+                    arr[row][col] = k;
+                } else if (col == arr.length - 1) {
+                    row++;
+                    k++;
+                    directioUp = false;
+                    arr[row][col] = k;
+                } else {
+                    col++;
+                    row--;
+                    k++;
+                    arr[row][col] = k;
+                }
+            }
+            else if (!directioUp) {
+                if (col ==0 && row < n-1 ) {
+                    row++;
+                    directioUp = true;
+                    k++;
+                    arr[row][col] = k;
+                }
+                else if (row == arr.length - 1) {
+                    col++;
+                    directioUp = true;
+                    k++;
+                    arr[row][col] = k;
+                }
+                else {
+                    row++;
+                    col--;
+                    k++;
+                    arr[row][col] = k;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + "\t");
+
+            }
+            System.out.println();
+        }
     }
 }
